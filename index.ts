3e717772257bfd2ea23976e92616419c57ac2f69
HoaiNam1511 <90500710+HoaiNam1511@users.noreply.github.com>
2023-05-25 21:30:29 +0700

    add cors local

--- index.ts
+++ index.ts
@@ -12,35 +12,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req: Request, res: Response, next: NextFunction) {
-    // Website you wish to allow to connect
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://pizza-restaurant-fe.vercel.app'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://pizza-restaurant-beta.vercel.app'
-    );
 
-    // Request methods you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    );
-
-    // Request headers you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With,content-type'
-    );
-
-    // Pass to next layer of middleware
-    next();
-});
 app.use(
     cors({
         origin: [
+            'http://localhost:3000',
+            'http://localhost:3001',
             'https://pizza-restaurant-fe.vercel.app',
             'https://pizza-restaurant-beta.vercel.app',
         ],
